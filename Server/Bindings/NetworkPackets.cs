deee379d6e5200f7ff5ccd348bf913b4cb31f524
Pennyr <renemifsud93@gmail.com>
2019-03-24 11:33:50 +0100

    Add files via upload
    C# server

---
+++ Server/Bindings/NetworkPackets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindings
+{
+  // get send from servedr to client
+  //  client has to listen to serverpackets
+   public enum ServerPackets
+   {
+    SConnectionOk =1,
+   }
+
+    // get send from client to server
+    // server has to listen to clientpackets
+   public enum ClientPackets
+   {
+        CThankyou =1,
+   }
+}
